6b540276e42cdbd23a20a4fe451e4679d511e5fc
Tony Ruiz <truizo@outlook.com>
2021-02-17 07:10:27 -0500

    added content

---
+++ script.js
@@ -0,0 +1,4 @@
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const steps = document.querySelectorAll(".step");
